2dc65036c8cc36702eb5f352eb6981aca274c634
Anton Makarevich <anton.makarevich@hotmail.com>
2023-06-06 22:26:47 +0200

    Add DI extension for avalonia

---
+++ DI/Sanet.MVVM.DI.Avalonia/Extensions/AppBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Sanet.MVVM.DI.Avalonia.Extensions;
+
+public static class AppBuilderExtensions
+{
+    public static AppBuilder UseDependencyInjection(this AppBuilder appBuilder, Action<IServiceCollection> registerServicesAction)
+    {
+        if (Design.IsDesignMode) { return appBuilder; }
+        
+        appBuilder.AfterSetup(x =>
+        {
+            var services = new ServiceCollection();
+
+            registerServicesAction(services);
+            
+            x.Instance?.Resources.Add(
+                ServiceCollectionResourceKey,
+                services);
+        });
+
+        return appBuilder;
+    }
+    public const string ServiceCollectionResourceKey = $"Sanet.MVVM.DI.Avalonia.{nameof(ServiceCollectionResourceKey)}";
+}
